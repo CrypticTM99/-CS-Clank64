d2e6ec2df8eeb81844b879493be10bc09b6b5517
CrypticTM99 <67986767+CrypticTM99@users.noreply.github.com>
2026-06-17 03:25:24 -0300

    adding [actors] folder

---
+++ actors/w_test3/texscroll.inc.h
@@ -0,0 +1,4 @@
+extern void scroll_w_test3_Torso_mesh_layer_1_vtx_0();
+extern void scroll_w_test3_Head_DL_mesh_layer_1_vtx_0();
+extern void scroll_gfx_mat_w_test3_Material__345_f3d();
+extern void scroll_geo_w_test3();
